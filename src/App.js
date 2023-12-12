f60607f06e9b1850ea606d9fdfbed3e72b2e6a0d
tauri_s <tauristclaire@gmail.com>
2023-12-12 10:43:13 -0800

    Remove extra closing bracket at bottom of App component and fix indentation structure

--- src/App.js
+++ src/App.js
@@ -7,22 +7,21 @@ const App = () => {
     setIsOn();
   };
 
-    let wallClass = "wall off";
-    if (this.state.on) {
-      props.toggleLight(isOn);
-    }
-    return (
-      <div className={wallClass}>
-        <div className="switch-plate">
-          <div className="screw" />
-          <div className="switch" onClick={toggleLight}>
-            <div className="switch-handle" />
-          </div>
-          <div className="screw" />
-        </div>
-      </div>
-    )
+  let wallClass = "wall off";
+  if (this.state.on) {
+    props.toggleLight(isOn);
   }
+  return (
+    <div className={wallClass}>
+      <div className="switch-plate">
+        <div className="screw" />
+        <div className="switch" onClick={toggleLight}>
+          <div className="switch-handle" />
+        </div>
+        <div className="screw" />
+      </div>
+    </div>
+  )
 }
 
 export default App;
