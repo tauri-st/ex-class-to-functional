fa3c98871732c4c7807bdd2af9eceb756293623d
tauri_s <tauristclaire@gmail.com>
2024-01-24 16:19:55 -0800

    Add previous code that was converted back in in notes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ const App = () => {
   const [isOn, setIsOn] = useState(true);
 
   const toggleLight = () => {
+    // const isOn = this.state.on;
+    // this.setState({ on: !isOn });
     setIsOn(!isOn);
   };
 
+  //render() {
   let wallClass = "wall off";
   // Update the toggleLight function to work in a functional 
   // component and to correctly update the state value using 
   // the hooks syntax. Hint: You don’t need “this” because this 
   // isn’t a class component anymore.
+  /* if (this.state.on) {
+    wallClass = "wall on";
+  }*/
   if (isOn) {
     wallClass="wall on";
   }
@@ -19,6 +25,7 @@ const App = () => {
     <div className={wallClass}>
       <div className="switch-plate">
         <div className="screw" />
+        {/*<div className="switch" onClick={this.toggleLight}> */}
         <div className="switch" onClick={toggleLight}>
           <div className="switch-handle" />
         </div>
